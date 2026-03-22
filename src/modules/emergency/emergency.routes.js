7e85948fe94574ef3f658faa82cc491e95ac7ac4
daniel69zz <luisdanielrojascaceres@gmail.com>
2026-03-22 18:46:45 -0400

    email + video

--- src/modules/emergency/emergency.routes.js
+++ src/modules/emergency/emergency.routes.js
@@ -4,5 +4,6 @@ const emergencyController = require('./emergency.controller');
 const router = express.Router();
 
 router.post('/send-email', emergencyController.sendEmailValidation, emergencyController.sendEmail);
+router.get('/evidence/:id/url', emergencyController.getEvidenceUrl);
 
 module.exports = router;
